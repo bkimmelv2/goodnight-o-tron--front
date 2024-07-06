469422e15a191c9474ce57be11c4d0d63d5fbdd1
Baylee Kimmel <bkimmel1226@gmail.com>
2024-07-05 23:55:24 -0400

    Header, Main, and Footer components added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = (props) => {
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Goodnight-o-tron by Kimmel, Inc.</p>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+
+
+const Main = (props) => {
+    return (
+        <div>
+            <h1>Main Placeholder</h1>
+        </div>
+    )
+}
+
+export default Main
