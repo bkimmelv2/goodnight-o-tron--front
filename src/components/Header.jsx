a68a14c0ee5a5fc40eb54f96e31d8ee06d30e9a0
Baylee Kimmel <bkimmel1226@gmail.com>
2024-07-08 23:03:37 -0400

    Tailwind added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 const Header = (props) => {
     return (
         <div>
-            <h1>Goodnight-o-Tron</h1>
+            <h1 className="text-3xl font-bold underline">Goodnight-o-Tron</h1>
             <nav>
                 <Link to='/'><button>HOME</button></Link>
                 <Link to='/books'><button>LIBRARY</button></Link>
